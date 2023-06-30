2e47f70bc10eb91d4de67c3ecd58c6f7b6bacf0d
Rogerio Almeida de Mattos <rogeriomattos38@outlook.com>
2023-06-30 18:03:29 -0300

    Aulas 1 - 2

---
+++ testes-e2e-cypress-main/cypress/e2e/inicio.cy.js
@@ -0,0 +1,8 @@
+describe('Pagina inicial', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+  it('Deve renderizar o h1 com o texto correto', () => {
+    cy.getByData('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!');
+  })
+})
